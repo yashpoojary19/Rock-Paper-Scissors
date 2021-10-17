0f5272cf0474e71930387e3548a0ef117b342c8a
Yash Poojary <yashspoojary245@gmail.com>
2021-10-17 16:22:02 +0530

    Initial Commit

---
+++ Rock Paper Scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissors
+//
+//  Created by Yash Poojary on 17/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Yash Poojary on 17/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
